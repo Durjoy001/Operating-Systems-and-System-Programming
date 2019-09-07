d6efe206eeb7cd5b68d3a9edb6cac7fadfa99102
Durjoy Dey <43565286+Durjoy001@users.noreply.github.com>
2019-09-08 00:47:35 +0600

    Add files via upload

---
+++ RoomChat_Client/src/Client/RoomChat2.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Client;
+
+//import roomchat2.ClientLogin;
+
+/**
+ *
+ * @author foysal
+ */
+public class RoomChat2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+        ClientLogin first=new ClientLogin();
+        
+        first.main(args);
+      
+//      System.out.println(s);
+        
+    }
+    
+}
